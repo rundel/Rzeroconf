9ba70fc2c2de31de5ff7d61d96a8dfbf779c4b66
Colin Rundel <rundel@gmail.com>
2018-11-04 15:35:50 -0500

    Remove servus

--- src/Rzeroconf.cpp
+++ src/Rzeroconf.cpp
@@ -1,18 +1,5 @@
 #include <Rcpp.h>
-#include "servus.h"
 
-void print_res (std::vector<std::string> const& v) {
-  
-  if (v.size() == 0) {
-    Rcpp::Rcout << "<empty>";
-  } else {
-    for(auto const& e : v) {
-      Rcpp::Rcout << e << " ";
-    }
-  }
-  
-  Rcpp::Rcout << "\n";
-}
 
 //' @export
 // [[Rcpp::export]]
